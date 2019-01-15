8dfa22fd70ffc655a93c28cf5558f0b7140dcc3d
eisverticker <eisverticker@posteo.de>
2019-01-15 09:40:50 +0100

    Project setup (python with python-kafka and logger)

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # web-disseminator
 
 Ein Warnmeldungsmultiplkator, welche die Nachricht vom Message-Broker entgegennimmt und an verbundene Websocket-Clients pusht.
+
 Dieses Projekt ist Teil der Masterarbeit von Alexander Noack (TU Ilmenau; Fachgebiet Softwarearchitekturen und Produktlinien).
 
+## build
+
+```bash
+docker build -t web-disseminator .
+```
---
+++ src/python/app.py
@@ -0,0 +1,25 @@
+import logging
+
+from kafka import KafkaConsumer
+ 
+def createLogger(category):
+    logger = logging.getLogger('kafka_logger')
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+if __name__ == '__main__':
+    logger = createLogger('main')
+    topic = 'birds'
+    logger.info('using topic birds')
+    consumer = KafkaConsumer(topic, auto_offset_reset='earliest', bootstrap_servers=['kafka:9092'])
+    logger.info('subscribe')
+    for message in consumer:
+        logger.info("Eine Nachricht aus dem Topic " + topic + " ist angekommen")
+
+    if consumer is not None:
+        consumer.close()
